3d3cde9b1849351fef360d24573ef03fac3bdaa2
Mitchell Wills <mwills@wpi.edu>
2014-04-23 16:55:31 -0400

    Revert "updated drive roboteq controller to use effort instead of velocity"
    This reverts commit 3f817b63112e3de395f83cdeb77b099415b8b3fc.

--- aero_hw/include/aero_hw/aero_base_hw.h
+++ aero_hw/include/aero_hw/aero_base_hw.h
@@ -27,12 +27,12 @@ public:
     vector<std::string> drive_names = list_of(robot_ns+"joint_front_left_wheel")(robot_ns+"joint_front_right_wheel");
     drive_trans = list_of<transmission_interface::SimpleTransmission>(1.0)(1.0);
 
-    drive_motor_controller = boost::shared_ptr<RoboteqControllerHW>(new RoboteqControllerHW("/dev/MTR", drive_names[0], 10, 1, drive_names[1], 10, 1, act_state_interface, act_eff_interface));
+    drive_motor_controller = boost::shared_ptr<RoboteqControllerHW>(new RoboteqControllerHW("/dev/MTR", drive_names[0], 10, 1, drive_names[1], 10, 1, act_state_interface, act_vel_interface));
     
 
     for(int i = 0; i<drive_names.size(); ++i){
       ROS_INFO_STREAM(drive_names[i]);
-      hardware_interface::ActuatorHandle actuator_handle = act_eff_interface.getHandle(drive_names[i]);
+      hardware_interface::ActuatorHandle actuator_handle = act_vel_interface.getHandle(drive_names[i]);
       act_to_jnt_state.registerHandle(transmission_interface::ActuatorToJointStateHandle(drive_names[i],
                                                                  &drive_trans[i],
   							         river_ros_util::create_transmission_actuator_state_data(actuator_handle),
@@ -40,25 +40,25 @@ public:
 
       transmission_interface::ActuatorData a_cmd_data;
       transmission_interface::JointData j_cmd_data;
-      hardware_interface::ActuatorHandle actuator_cmd_handle = act_eff_interface.getHandle(drive_names[i]);
-      j_cmd_data.effort.push_back(&drive_joint_data[i].cmd);
-      a_cmd_data.effort.push_back((double*)actuator_cmd_handle.getCommandPtr());
-      jnt_to_act_eff.registerHandle(transmission_interface::JointToActuatorEffortHandle(drive_names[i],
+      hardware_interface::ActuatorHandle actuator_cmd_handle = act_vel_interface.getHandle(drive_names[i]);
+      j_cmd_data.velocity.push_back(&drive_joint_data[i].cmd);
+      a_cmd_data.velocity.push_back((double*)actuator_cmd_handle.getCommandPtr());
+      jnt_to_act_vel.registerHandle(transmission_interface::JointToActuatorVelocityHandle(drive_names[i],
   								 &drive_trans[i],
   								 a_cmd_data,
   								 j_cmd_data));
 
 
       jnt_state_interface.registerHandle(drive_joint_data[i].state_handle(drive_names[i]));
-      jnt_eff_interface.registerHandle(hardware_interface::JointHandle(jnt_state_interface.getHandle(drive_names[i]), &drive_joint_data[i].cmd));
+      jnt_vel_interface.registerHandle(hardware_interface::JointHandle(jnt_state_interface.getHandle(drive_names[i]), &drive_joint_data[i].cmd));
     }
 
     registerInterface(&act_state_interface);
-    registerInterface(&act_eff_interface);
+    registerInterface(&act_vel_interface);
     registerInterface(&jnt_state_interface);
-    registerInterface(&jnt_eff_interface);
+    registerInterface(&jnt_vel_interface);
     registerInterface(&act_to_jnt_state);
-    registerInterface(&jnt_to_act_eff);
+    registerInterface(&jnt_to_act_vel);
   }
 
   void read(){
@@ -67,17 +67,17 @@ public:
   }
   void write(){
     drive_motor_controller->write();
-    jnt_to_act_eff.propagate();
+    jnt_to_act_vel.propagate();
   }
 
  private:
   boost::shared_ptr<RoboteqControllerHW> drive_motor_controller;  
   hardware_interface::ActuatorStateInterface act_state_interface;
-  hardware_interface::EffortActuatorInterface act_eff_interface;
+  hardware_interface::VelocityActuatorInterface act_vel_interface;
   hardware_interface::JointStateInterface jnt_state_interface;
-  hardware_interface::EffortJointInterface jnt_eff_interface;
+  hardware_interface::VelocityJointInterface jnt_vel_interface;
   transmission_interface::ActuatorToJointStateInterface act_to_jnt_state;
-  transmission_interface::JointToActuatorEffortInterface jnt_to_act_eff;
+  transmission_interface::JointToActuatorVelocityInterface jnt_to_act_vel;
 
   std::vector<transmission_interface::SimpleTransmission> drive_trans;
 
