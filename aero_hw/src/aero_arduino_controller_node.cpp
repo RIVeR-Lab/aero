ee9250c13debcdf0cd33f3fdf934ca61d08fa4ee
Velin Dimitrov <velinddimitrov@gmail.com>
2014-06-07 16:26:23 -0400

    Added 5 second delay when unpausing to remove jitter on pause line

--- aero_hw/src/aero_arduino_controller_node.cpp
+++ aero_hw/src/aero_arduino_controller_node.cpp
@@ -1,5 +1,3 @@
-
-
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
 #include <safety_interface/SoftwareStop.h>
@@ -15,16 +13,31 @@ static ros::Subscriber arduino_pause_sub;
 
 bool last_pause_state = true;
 bool is_first_time = true;
+
+double time_of_toggle = 0;
+
+
 void pauseCallback(const std_msgs::BoolConstPtr& is_paused){
   ROS_DEBUG_STREAM("Reveived software stop message from arduino: "<<is_paused->data);
+  
   if(is_paused->data!=last_pause_state || is_first_time){
     safety_interface::SoftwareStop msg;
     msg.stop = is_paused->data;
     msg.header.stamp = ros::Time::now();
+    time_of_toggle = msg.header.stamp.toSec();
+	    
     if(msg.stop)
       msg.message = "Hardware pause was pressed";
+    
     else
       msg.message = "Hardware pause was released";
+  		if(ros::Time::now().toSec() - time_of_toggle < 5 && !is_first_time){
+  			msg.message = "Hardware pause is released, but delay is in effect";
+  			msg.stop = true;
+  		}
+  		else
+  			msg.message = "Hardware pause was pressed";	
+
     last_pause_state = msg.stop;
     is_first_time = false;
     pause_pub.publish(msg);
